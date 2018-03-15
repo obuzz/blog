e7d108a5cec7a645d02e0f536976a3cf56816861
Obuzz <odprew@gmail.com>
2018-03-15 09:45:53 +0000

    generate links scaffold

---
+++ app/views/links/_link.json.jbuilder
@@ -0,0 +1,2 @@
+json.extract! link, :id, :title, :string, :url, :string, :created_at, :updated_at
+json.url link_url(link, format: :json)
---
+++ db/migrate/20180315092821_create_links.rb
@@ -0,0 +1,12 @@
+class CreateLinks < ActiveRecord::Migration[5.1]
+  def change
+    create_table :links do |t|
+      t.string :title
+      t.string :string
+      t.string :url
+      t.string :string
+
+      t.timestamps
+    end
+  end
+end
